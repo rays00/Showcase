9e5408af1bcc02784370fecce421c48222c87f62
Dan Brezeanu <bdangabriel@gmail.com>
2023-08-12 14:05:58 +0300

    Add homepage component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomepageComponent],
+  imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
