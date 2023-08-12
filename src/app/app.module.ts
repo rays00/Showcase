7d89b7c0b7258ab9b2f3e095793705342731b9da
Dan Brezeanu <bdangabriel@gmail.com>
2023-08-12 14:42:27 +0300

    Add images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from "./app.component";
 import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [AppComponent]
 })
